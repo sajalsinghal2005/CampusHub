4dbdf80b751c3cda2cdabc23967ce1917ef6d4c4
sajalsinghal2000 <b241226@skit.ac.in>
2026-08-08 23:14:37 +0530

    complete student CRUD APIs

--- backend/students/serializers.py
+++ backend/students/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from.models import  Student
+from .models import Student
+
 
 class StudentSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Student
-        fields = "__all__"
+        fields = [
+            "id",
+            "user",
+            "branch",
+            "semester",
+            "cgpa",
+            "phone",
+            "github",
+            "linkedin",
+            "resume",
+        ]
+        read_only_fields = ["id", "user"]
--- backend/students/views.py
+++ backend/students/views.py
@@ -1,10 +1,14 @@
-from rest_framework import  viewsets
-from rest_framework.permissions  import IsAuthenticated
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-from  .models  import  Student
+from .models import Student
 from .serializers import StudentSerializer
+
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    permission_classes  = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
